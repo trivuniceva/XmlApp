0b147d48c5b3d93086ead94594601308daab6e74
trivuniceva <trivunic99@gmail.com>
2025-02-26 16:58:41 +0100

    sad zaista radi

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/login/login.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import {HomeComponent} from './features/pages/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
 ];
