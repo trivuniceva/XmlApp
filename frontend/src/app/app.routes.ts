b835bd71227214325a5c26f9da6e717fbaf02f66
trivuniceva <trivunic99@gmail.com>
2025-02-26 19:27:13 +0100

    refactor

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/pages/login/login.component';
-import {HomeComponent} from './features/pages/home/home.component';
+import {HomeComponent} from './features/home/home.component';
 import {SignupComponent} from './features/auth/pages/signup/signup.component';
 import {BasicSearchComponent} from './features/search/basic-search/basic-search.component';
 
