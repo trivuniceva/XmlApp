f1bffa2473a13d11ad977ed8698ccab9189f96b8
trivuniceva <trivunic99@gmail.com>
2025-02-26 14:01:02 +0100

    neki sam problem sa frontom napravila

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from "./features/navbar/navbar.component";
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'client';
 }
