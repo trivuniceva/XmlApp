fca634fcde37edf34e9c7628e2a222d156c135e1
trivuniceva <trivunic99@gmail.com>
2025-02-26 19:26:18 +0100

    Create basic-search.component.ts

---
+++ frontend/src/app/features/search/basic-search/basic-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './basic-search.component.html',
+  styleUrl: './basic-search.component.css'
+})
+export class BasicSearchComponent {
+
+}
