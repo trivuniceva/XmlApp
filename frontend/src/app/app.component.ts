2b612590ff5ef689728cc19face4e699a4a7ccac
trivuniceva <trivunic99@gmail.com>
2025-02-26 19:21:22 +0100

    refactor

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {RouterModule, RouterOutlet} from '@angular/router';
-import {NavbarComponent} from './features/navbar/navbar.component';
+import {NavbarComponent} from './shared/components/navbar/navbar.component';
 import {CommonModule} from '@angular/common';
 
 @Component({
