0eaeedea82c29c7c4d9a2b4bfed98dd082647ebf
trivuniceva <trivunic99@gmail.com>
2025-02-26 14:49:20 +0100

    sad konacno front radi...

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {RouterModule, RouterOutlet} from '@angular/router';
+import {NavbarComponent} from './features/navbar/navbar.component';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    NavbarComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'frontend';
+}
