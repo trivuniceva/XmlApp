286e8b668e18b8f98b1fa6ba2e207531fc19e776
trivuniceva <trivunic99@gmail.com>
2025-02-26 17:28:22 +0100

    login component

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {LoginFormComponent} from '../components/login-form/login-form.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    LoginFormComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private router: Router) { }
+
+  forgotPassword() {
+
+  }
+
+  goToSignup() {
+
+  }
+
+  handleLogin($event: any) {
+    this.router.navigate(['/signup'])
+  }
 }
