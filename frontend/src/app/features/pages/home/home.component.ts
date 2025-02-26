9380ce1a5f0c101688c50020038fd1a196267ec5
trivuniceva <trivunic99@gmail.com>
2025-02-26 17:32:18 +0100

    signup form and page

--- frontend/src/app/features/pages/home/home.component.ts
+++ frontend/src/app/features/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+
+  constructor(private router: Router) {
+  }
+
+  goToSignup() {
+    this.router.navigate(['/signup'])
+  }
 }
