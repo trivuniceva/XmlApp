a65091d6a34089ffe2076d8706c4f0798d5cfbe7
trivuniceva <trivunic99@gmail.com>
2025-02-26 19:26:25 +0100

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import {HomeComponent} from './features/pages/home/home.component';
 import {SignupComponent} from './features/auth/pages/signup/signup.component';
+import {BasicSearchComponent} from './features/search/basic-search/basic-search.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'search', component: BasicSearchComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
