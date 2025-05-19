14a1a9e905f4d50240cc88460debdd5fa3ef5b91
trivuniceva <trivunic99@gmail.com>
2025-05-19 23:40:14 +0200

    copyright info

--- frontend/src/app/core/model/FizickoPodnosilac.ts
+++ frontend/src/app/core/model/FizickoPodnosilac.ts
@@ -5,5 +5,16 @@ export interface FizickoPodnosilac {
   userInfo: UserInfo;
   isAuthorSubmitting: boolean;
   authors: AuthorInfo[];
+  copyrightInfo: { // Dodajemo informacije o delu
+    workType: string;
+    otherWorkType?: string;
+    title: string;
+    altTitle?: string;
+    recordFormat: string;
+    otherRecordFormat?: string;
+    isAdaptation: boolean | null;
+    sourceTitle?: string;
+    sourceAuthorInfo?: AuthorInfo; // Informacije o autoru originalnog dela
+  };
 
 }
--- frontend/src/app/features/submit-request/submit-request-page/submit-request-page.component.ts
+++ frontend/src/app/features/submit-request/submit-request-page/submit-request-page.component.ts
@@ -1,20 +1,19 @@
-import {Component, ViewChild} from '@angular/core';
-import {HeaderComponent} from "../../request-template/components/header/header.component";
-import {CommonModule, NgIf} from '@angular/common';
-import {AuthorInfoComponent} from '../../request-template/components/author-info/author-info.component';
-import {
-  ApplicantIdentificationComponent
-} from '../components/applicant-identification/applicant-identification.component';
-import {FizickoFormComponent} from '../components/fizicko-form/fizicko-form.component';
-import {PravnoFormComponent} from '../components/pravno-form/pravno-form.component';
-import {PunomocnikFormComponent} from '../components/punomocnik-form/punomocnik-form.component';
-import {CopyrightInfoFormComponent} from '../components/copyright-info-form/copyright-info-form.component';
-import {AttachmentsComponent} from '../components/attachments/attachments.component';
-import {ConfirmationDialogComponent} from '../components/confirmation-dialog/confirmation-dialog.component';
-import {PravnoPodnosilac} from '../../../core/model/PravnoPodnosilac';
-import {PunomocnikComponent} from '../../request-template/components/punomocnik/punomocnik.component';
-import {PunomocnikPodnosilac} from '../../../core/model/PunomocnikPodnosilac';
-import {FizickoPodnosilac} from '../../../core/model/FizickoPodnosilac';
+import { Component, ViewChild } from '@angular/core';
+import { HeaderComponent } from "../../request-template/components/header/header.component";
+import { CommonModule, NgIf } from '@angular/common';
+import { AuthorInfoComponent } from '../../request-template/components/author-info/author-info.component';
+import { ApplicantIdentificationComponent } from '../components/applicant-identification/applicant-identification.component';
+import { FizickoFormComponent } from '../components/fizicko-form/fizicko-form.component';
+import { PravnoFormComponent } from '../components/pravno-form/pravno-form.component';
+import { PunomocnikFormComponent } from '../components/punomocnik-form/punomocnik-form.component';
+import { CopyrightInfoFormComponent } from '../components/copyright-info-form/copyright-info-form.component';
+import { AttachmentsComponent } from '../components/attachments/attachments.component';
+import { ConfirmationDialogComponent } from '../components/confirmation-dialog/confirmation-dialog.component';
+import { PravnoPodnosilac } from '../../../core/model/PravnoPodnosilac';
+import { PunomocnikComponent } from '../../request-template/components/punomocnik/punomocnik.component';
+import { PunomocnikPodnosilac } from '../../../core/model/PunomocnikPodnosilac';
+import { FizickoPodnosilac } from '../../../core/model/FizickoPodnosilac';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-submit-request-page',
@@ -35,19 +34,20 @@ import {FizickoPodnosilac} from '../../../core/model/FizickoPodnosilac';
   templateUrl: './submit-request-page.component.html',
   styleUrl: './submit-request-page.component.css'
 })
+
 export class SubmitRequestPageComponent {
   currentStep: number = 1;
   applicantType: string | null = null;
   showPopup = false;
 
-  fizickoFormData: FizickoPodnosilac | undefined;
+  fizickoFormData: FizickoPodnosilac | null = null;
   pravnoFormData: PravnoPodnosilac | undefined;
   punomocnikFormData: PunomocnikPodnosilac | undefined;
 
   @ViewChild('fizickoFormRef') fizickoFormComponent?: FizickoFormComponent;
   @ViewChild('pravnoFormRef') pravnoFormComponent?: PravnoFormComponent;
   @ViewChild('punomocnikFormRef') punomocnikFormComponent?: PunomocnikFormComponent;
-
+  @ViewChild('copyrightInfoFormRef') copyrightInfoFormComponent?: CopyrightInfoFormComponent;
 
   goToStep(step: number) {
     this.currentStep = step;
@@ -60,23 +60,19 @@ export class SubmitRequestPageComponent {
 
   collectFormData() {
     if (this.pravnoFormComponent) {
-      this.pravnoFormData = this.pravnoFormComponent.getFormValue() as PravnoPodnosilac; // Castuj na PravnoPodnosilac
+      this.pravnoFormData = this.pravnoFormComponent.getFormValue() as PravnoPodnosilac;
       console.log("Podaci pravnog lica prikupljeni:", this.pravnoFormData);
       this.goToStep(3);
-    }
-
-    else if (this.punomocnikFormComponent) {
+    } else if (this.punomocnikFormComponent) {
       this.punomocnikFormData = this.punomocnikFormComponent.getFormValue() as PunomocnikPodnosilac;
-      console.log(this.punomocnikFormData)
+      console.log(this.punomocnikFormData);
       this.goToStep(3);
-    }
-
-    else if (this.fizickoFormComponent) {
-      const formValue = this.fizickoFormComponent.getFormValue();
+    } else if (this.fizickoFormComponent) {
+      const fizickoFormValue = this.fizickoFormComponent.getFormValue();
       this.fizickoFormData = {
-        userInfo: formValue.userInfo,
-        isAuthorSubmitting: this.fizickoFormComponent.isAuthorSubmitting, // Pristup isAuthorSubmitting
-        authors: (formValue.authors as any[]).map(author => ({ // Mapiramo podatke autora
+        userInfo: fizickoFormValue.userInfo,
+        isAuthorSubmitting: this.fizickoFormComponent.isAuthorSubmitting,
+        authors: (fizickoFormValue.authors as any[]).map(author => ({
           name: author.name,
           lastname: author.lastname,
           city: author.city,
@@ -87,28 +83,39 @@ export class SubmitRequestPageComponent {
           email: author.email,
           isAnonymousAuthor: author.isAnonymousAuthor
         })),
+        copyrightInfo: {} as any // Inicijalizujemo prazan objekat, popunicemo kasnije
       };
-      console.log(this.fizickoFormData);
+      console.log("Podaci o podnosiocu (fizicko) prikupljeni:", this.fizickoFormData);
       this.goToStep(3);
     }
+  }
 
-
+  collectCopyrightInfo() {
+    if (this.copyrightInfoFormComponent && this.fizickoFormData) {
+      const copyrightFormValue = this.copyrightInfoFormComponent.copyrightInfoForm.value;
+      this.fizickoFormData.copyrightInfo = {
+        workType: copyrightFormValue.workType,
+        otherWorkType: copyrightFormValue.otherWorkType,
+        title: copyrightFormValue.title,
+        altTitle: copyrightFormValue.altTitle,
+        recordFormat: copyrightFormValue.recordFormat,
+        otherRecordFormat: copyrightFormValue.otherRecordFormat,
+        isAdaptation: copyrightFormValue.isAdaptation,
+        sourceTitle: copyrightFormValue.sourceTitle,
+        sourceAuthorInfo: copyrightFormValue.sourceAuthorInfo
+      };
+      console.log("Podaci o delu prikupljeni:", this.fizickoFormData.copyrightInfo);
+      this.goToStep(4);
+    }
   }
 
   onSubmitForm() {
     if (this.pravnoFormData) {
       console.log("Podaci pravnog lica za submit:", this.pravnoFormData);
-      console.log("Poslovno ime:", this.pravnoFormData.poslovnoIme);
-      console.log("Ime korisnika:", this.pravnoFormData.userInfo.name);
-    }
-
-    else if (this.punomocnikFormData){
-      console.log(this.punomocnikFormData)
-    }
-
-    else if (this.fizickoFormData){
-      console.log(this.fizickoFormData)
-      console.log("Autori:", this.fizickoFormData.authors);
+    } else if (this.punomocnikFormData) {
+      console.log(this.punomocnikFormData);
+    } else if (this.fizickoFormData) {
+      console.log("Finalni podaci za fizicko lice:", this.fizickoFormData);
     }
 
     this.showPopup = true;
