e0a439f0e6f18a1bb3f1f8cee8b908039e7a099f
trivuniceva <trivunic99@gmail.com>
2025-05-20 18:04:26 +0200

    punomocnik

--- frontend/src/app/core/model/PunomocnikPodnosilac.ts
+++ frontend/src/app/core/model/PunomocnikPodnosilac.ts
@@ -5,4 +5,15 @@ import {AuthorInfo} from './AuthorInfo';
 export interface PunomocnikPodnosilac {
   userInfo: UserInfo;
   authorInfo: AuthorInfo;
+  copyrightInfo: {
+    workType: string;
+    otherWorkType?: string;
+    title: string;
+    altTitle?: string;
+    recordFormat: string;
+    otherRecordFormat?: string;
+    isAdaptation: boolean | null;
+    sourceTitle?: string;
+    sourceAuthorInfo?: AuthorInfo;
+  };
 }
--- frontend/src/app/features/submit-request/submit-request-page/submit-request-page.component.ts
+++ frontend/src/app/features/submit-request/submit-request-page/submit-request-page.component.ts
@@ -42,7 +42,7 @@ export class SubmitRequestPageComponent {
   showPopup = false;
 
   fizickoFormData: FizickoPodnosilac | null = null;
-  pravnoFormData: PravnoPodnosilac | undefined;
+  pravnoFormData: PravnoPodnosilac | null = null;
   punomocnikFormData: PunomocnikPodnosilac | undefined;
 
   @ViewChild('fizickoFormRef') fizickoFormComponent?: FizickoFormComponent;
@@ -66,13 +66,17 @@ export class SubmitRequestPageComponent {
     if (this.pravnoFormComponent) {
       const formData = this.pravnoFormComponent.getFormValue() as PravnoPodnosilac;
       this.formDataService.setPravnoFormData(formData);
+      this.pravnoFormData = formData; // Dodajte ovu liniju da sačuvate podatke o pravnom licu
+      this.fizickoFormData = null; // Postavite na null kada je pravno lice
+      this.punomocnikFormData = undefined; // Postavite na undefined kada je pravno lice
       console.log("Podaci pravnog lica prikupljeni i poslati servisu:", formData);
-
-
       this.goToStep(3);
     } else if (this.punomocnikFormComponent) {
       const formData = this.punomocnikFormComponent.getFormValue() as PunomocnikPodnosilac;
       this.formDataService.setPunomocnikFormData(formData);
+      this.punomocnikFormData = formData; // Dodajte ovu liniju da sačuvate podatke o punomoćniku
+      this.fizickoFormData = null; // Postavite na null kada je punomoćnik
+      this.pravnoFormData = null; // Postavite na null kada je punomoćnik
       console.log("Podaci punomocnika prikupljeni i poslati servisu:", formData);
       this.goToStep(3);
     } else if (this.fizickoFormComponent) {
@@ -95,6 +99,8 @@ export class SubmitRequestPageComponent {
       };
       this.formDataService.setFizickoFormData(formData);
       this.fizickoFormData = formData;
+      this.pravnoFormData = null; // Postavite na null kada je fizičko lice
+      this.punomocnikFormData = undefined; // Postavite na undefined kada je fizičko lice
       console.log("Podaci o podnosiocu (fizicko) prikupljeni i poslati servisu:", this.fizickoFormData);
       this.goToStep(3);
     }
@@ -103,28 +109,62 @@ export class SubmitRequestPageComponent {
   collectCopyrightInfo() {
     console.log("Pozvana collectCopyrightInfo()");
     console.log("this.copyrightInfoFormComponent:", this.copyrightInfoFormComponent);
-    console.log("this.fizickoFormData PRE AŽURIRANJA:", this.fizickoFormData);
 
-    if (this.copyrightInfoFormComponent && this.fizickoFormData) {
+    if (this.copyrightInfoFormComponent) {
       const copyrightFormValue = this.copyrightInfoFormComponent.copyrightInfoForm.value;
       console.log("copyrightFormValue:", copyrightFormValue);
 
-      this.fizickoFormData.copyrightInfo = {
-        workType: copyrightFormValue.workType,
-        otherWorkType: copyrightFormValue.otherWorkType,
-        title: copyrightFormValue.title,
-        altTitle: copyrightFormValue.altTitle,
-        recordFormat: copyrightFormValue.recordFormat,
-        otherRecordFormat: copyrightFormValue.otherRecordFormat,
-        isAdaptation: copyrightFormValue.isAdaptation,
-        sourceTitle: copyrightFormValue.sourceTitle,
-        sourceAuthorInfo: copyrightFormValue.sourceAuthorInfo
-      };
-      this.formDataService.setFizickoFormData(this.fizickoFormData);
-      console.log("Podaci o delu prikupljeni i ažurirani u servisu:", this.fizickoFormData);
-      this.goToStep(4);
+      if(this.fizickoFormData){
+        this.fizickoFormData.copyrightInfo = {
+          workType: copyrightFormValue.workType,
+          otherWorkType: copyrightFormValue.otherWorkType,
+          title: copyrightFormValue.title,
+          altTitle: copyrightFormValue.altTitle,
+          recordFormat: copyrightFormValue.recordFormat,
+          otherRecordFormat: copyrightFormValue.otherRecordFormat,
+          isAdaptation: copyrightFormValue.isAdaptation,
+          sourceTitle: copyrightFormValue.sourceTitle,
+          sourceAuthorInfo: copyrightFormValue.sourceAuthorInfo
+        }
+        this.formDataService.setFizickoFormData(this.fizickoFormData);
+        console.log("Podaci o delu prikupljeni i ažurirani u servisu (Fizicko):", this.fizickoFormData);
+        this.goToStep(4);
+      } else if (this.pravnoFormData){
+        this.pravnoFormData.copyrightInfo = {
+          workType: copyrightFormValue.workType,
+          otherWorkType: copyrightFormValue.otherWorkType,
+          title: copyrightFormValue.title,
+          altTitle: copyrightFormValue.altTitle,
+          recordFormat: copyrightFormValue.recordFormat,
+          otherRecordFormat: copyrightFormValue.otherRecordFormat,
+          isAdaptation: copyrightFormValue.isAdaptation,
+          sourceTitle: copyrightFormValue.sourceTitle,
+          sourceAuthorInfo: copyrightFormValue.sourceAuthorInfo
+        }
+        this.formDataService.setPravnoFormData(this.pravnoFormData);
+        console.log("Podaci o delu prikupljeni i ažurirani u servisu (Pravno):", this.pravnoFormData);
+        this.goToStep(4);
+      }
+
+      else if (this.punomocnikFormData) {
+        this.punomocnikFormData.copyrightInfo = {
+          workType: copyrightFormValue.workType,
+          otherWorkType: copyrightFormValue.otherWorkType,
+          title: copyrightFormValue.title,
+          altTitle: copyrightFormValue.altTitle,
+          recordFormat: copyrightFormValue.recordFormat,
+          otherRecordFormat: copyrightFormValue.otherRecordFormat,
+          isAdaptation: copyrightFormValue.isAdaptation,
+          sourceTitle: copyrightFormValue.sourceTitle,
+          sourceAuthorInfo: copyrightFormValue.sourceAuthorInfo
+        }
+        this.formDataService.setPunomocnikFormData(this.punomocnikFormData);
+        console.log("Podaci o delu prikupljeni i ažurirani u servisu (Punomocnik):", this.punomocnikFormData);
+        this.goToStep(4);
+      }
+
     } else {
-      console.warn("Nije moguće prikupiti podatke o delu. copyrightInfoFormComponent ili fizickoFormData nedostaje.");
+      console.warn("Nije moguće prikupiti podatke o delu. copyrightInfoFormComponent ili podaci o podnosiocu nedostaju.");
     }
   }
 
